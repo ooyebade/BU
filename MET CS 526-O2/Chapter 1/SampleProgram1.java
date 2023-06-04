d19c1e8d284cd41bda13c7465544e523c6f043ab
Elizabeth Oyebade <51129085+ooyebade@users.noreply.github.com>
2023-06-04 00:16:22 -0400

    Add files via upload

---
+++ MET CS 526-O2/Chapter 1/SampleProgram1.java
@@ -0,0 +1,13 @@
+package MyPrograms;
+
+public class SampleProgram1 {
+
+	//look for a main method
+	public static void main(String[] args) {
+
+		// execute the code
+		System.out.println("Has only main method");
+
+	}
+
+}
